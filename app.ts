1a2d5ae049fcce02088bf2debc369b5fff39187a
Mitchel Constantin <mythicalbox@weavver.com>
2021-12-01 19:54:06 -0800

    initial commit

---
+++ app.ts
@@ -0,0 +1,96 @@
+import Fastify from 'fastify'
+import Fastify_cors from 'fastify-cors'
+import { Static, Type } from '@sinclair/typebox'
+
+const fastify = Fastify({
+     logger: true
+});
+
+fastify.register(Fastify_cors, {});
+
+fastify.get('/', async (request, reply) => {
+     reply.type('application/json').code(200)
+
+     return {
+          // mbi: generateMBI(),
+          // valid_mbi: valid_mbi
+     }
+});
+
+fastify.listen(3000, (err, address) => {
+     if (err) throw err
+     // Server is now listening on ${address}
+})
+
+function getMBIFormat() {
+     let numeric_c = "123456789";
+     let numeric_n = "0123456789";
+     let alphabetic = "ACDEFGHJKMNPQRTUVWXY";
+
+     let mbi_format: string[] = [
+          numeric_c, // pos 1
+          alphabetic, // pos 2
+          alphabetic + numeric_n, // pos 3
+          numeric_n, // pos 4
+          alphabetic, // pos 5
+          alphabetic + numeric_n, // pos 6
+          numeric_n,  // pos 7
+          alphabetic,  // pos 8
+          alphabetic,  // pos 9
+          numeric_n,  // pos 10
+          numeric_n  // pos 11
+     ];
+
+     mbi_format
+     return mbi_format;
+}
+
+function generateMBI() {
+     let mbi_format : string[] = getMBIFormat();
+     let new_mbi : string = "";
+     for (var i = 0; i < mbi_format.length; i++) {
+          let format_for_position = mbi_format[i];
+          new_mbi += format_for_position[randomIntFromInterval(0, format_for_position.length - 1)];
+     }
+     new_mbi = insertIntoString(new_mbi, 4, "-");
+     new_mbi = insertIntoString(new_mbi, 8, "-");
+     return new_mbi;
+}
+
+fastify.get('/generate', async (request, reply) => {
+     return { mbi: generateMBI() };
+});
+
+function randomIntFromInterval(min, max) { // min and max included 
+     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function insertIntoString(str, index, value) {
+     return str.substr(0, index) + value + str.substr(index);
+}
+
+const ivalidate_post = Type.Object({
+     mbi: Type.String()
+});
+type ivalidate_post_type = Static<typeof ivalidate_post>;
+fastify.post<{ Body : ivalidate_post_type }>('/validate', async (request, reply) => {
+     let mbi : string = request.body.mbi;
+     console.log(request.body);
+     if (mbi == null || mbi == "")
+          return { valid: false, reason: "No input." };
+
+     mbi = mbi.replace(/-/g, "");
+     if (mbi.length != 11)
+          return { valid: false, reason: "Invalid length." };
+
+     let mbi_format : string[] = getMBIFormat();
+     
+     for (var i = 0; i < 11; i++) {
+          // console.log(mbi[i]);
+          if (mbi_format[i].indexOf(mbi[i]) == -1) {
+               return { valid: false, reason: "Character at position " + (i + 1) + " is invalid. (not counting dashes)" };
+          }
+     }
+
+     return { valid: true };
+});
---
+++ spa/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface mbi_response {
+     mbi : string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+     title : string = 'MBI Generator and Validator';
+     log : string = "";
+     mbi_to_validate : string = "";
+     validate_response : string = "";
+
+     constructor(private http: HttpClient) { }
+  
+     Generate() {
+          this.http.get<mbi_response>("http://localhost:3000/generate")
+               .subscribe((response : mbi_response) => {
+                    this.log = response.mbi + "<br />" + this.log;
+               });
+     }
+
+     Validate() {
+          this.http.post<mbi_response>("http://localhost:3000/validate", { mbi: this.mbi_to_validate })
+          .subscribe((response) => {
+               this.validate_response = JSON.stringify(response);
+          });
+     }
+}
